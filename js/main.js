f8854c6e62a6d34f53c914c30f879be45d060a7e
mateo-lundqvist-nti-johanneberg <119052302+mateo-lundqvist-nti-johanneberg@users.noreply.github.com>
2023-06-02 13:12:37 +0200

    done
    done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 let button = document.querySelector(".menu-button");
 let menu = document.querySelector(".menuthing");
+let menu2 = document.querySelector(".menuthing2");
 
 function openMenu(){
     menu.classList.toggle("showMenu");
+    menu2.classList.toggle("showMenu2");
     console.log(menu.className);
 }
 
